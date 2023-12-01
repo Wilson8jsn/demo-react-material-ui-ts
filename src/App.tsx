155494f41ca9e20a697ffdd2fcbf68ed295866d1
Wilson Castillo <dc105677543@gmail.com>
2023-12-01 06:14:42 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// En src/App.tsx
+import React, { useState } from 'react';
+import Login from './components/login';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { ThemeProvider, createTheme } from '@mui/material/styles'; // Importa ThemeProvider y createTheme
+
+const theme = createTheme({
+  // Agrega el fondo al tema global
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundImage: `url('https://res.cloudinary.com/dcba9lt2s/image/upload/v1701026662/samples/balloons.jpg')`, // Reemplaza con el enlace de tu imagen
+          backgroundSize: 'cover',
+          height: '100vh', // Ajusta según sea necesario
+          margin: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        },
+      },
+    },
+  },
+});
 
 function App() {
+  const [user, setUser] = useState<string | null>(null);
+
+  const handleLogin = (username: string, password: string) => {
+    setUser(username);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React11
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container component="main" maxWidth="xs">
+        <Login onLogin={handleLogin} />
+      </Container>
+    </ThemeProvider>
   );
 }
 
